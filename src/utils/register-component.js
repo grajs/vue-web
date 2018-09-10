6f0fa595ed7a916d85b4b03f3775b38956757bec
liujianhua <1769908045@qq.com>
2018-09-10 10:13:23 +0800

    feat:增加element-ui样式，增加了index.vue弹框及相应组件

--- src/utils/register-component.js
+++ src/utils/register-component.js
@@ -6,7 +6,10 @@ import {
   MenuItem,
   Submenu,
   Button,
-  Pagination
+  Pagination,
+  Dialog,
+  Input,
+  InputNumber
 } from 'element-ui'
 
 export default Vue => {
@@ -19,4 +22,7 @@ export default Vue => {
   Vue.component(Submenu.name, Submenu)
   Vue.component(Button.name, Button)
   Vue.component(Pagination.name, Pagination)
+  Vue.component(Dialog.name, Dialog)
+  Vue.component(Input.name, Input)
+  Vue.component(InputNumber.name, InputNumber)
 }
--- src/views/index.vue
+++ src/views/index.vue
@@ -1,5 +1,23 @@
 <template>
-  <div class="list-container">
+  <div class="index">
+    <el-dialog :visible.sync="dialogShow" title="添加商品" class="form-container">
+      <div class="form">
+        <el-input>
+          <template slot="prepend">名称</template>
+        </el-input>
+        <el-input placeholder="颜色">
+          <template slot="prepend">颜色</template>
+        </el-input>
+        <div class="input-number">
+          <label>数量</label>
+          <el-input-number :min="1" v-model="number" controls-position="right"></el-input-number>
+        </div>
+      </div>
+      <span slot="footer" class="dialog-footer">
+    <el-button @click="dialogShow = false">取 消</el-button>
+    <el-button type="primary" @click="dialogShow = false">确 定</el-button>
+  </span>
+    </el-dialog>
     <el-table :data="list" stripe style="width: 100%" :row-style="{textAlign:'center'}">
       <el-table-column prop="create_time" label="日期" width="150px"></el-table-column>
       <el-table-column prop="id" label="批次"></el-table-column>
@@ -20,6 +38,7 @@
                    layout="prev, pager, next"
                    :page-size="pageSize"
                    :total="120"></el-pagination>
+    <span ref="demo" id="demo">{{text}}</span>
   </div>
 </template>
 
@@ -30,7 +49,10 @@
     data() {
       return {
         list: [],
-        pageSize: 10
+        pageSize: 10,
+        dialogShow: false,
+        number: 1,
+        text: '243'
       }
     },
     created() {
@@ -39,20 +61,63 @@
         this.list = data
       })
     },
+    mounted() {
+    },
     methods: {
       getTime,
       handler(data) {
-        console.log(data)
+        this.dialogShow = true
       }
     }
   }
 </script>
 
-<style scoped>
-  .list-container {
-  }
-
-  .pager {
-    margin-top: 20px;
+<style lang="scss">
+  .index {
+    .form-container {
+      .el-dialog {
+        width: 600px;
+      }
+    }
+    .form {
+      text-align: center;
+      padding: 0 8%;
+      .el-input {
+        margin-bottom: 20px;
+        &:last-child {
+          margin-bottom: 0;
+        }
+      }
+    }
+    .input-number {
+      display: flex;
+      label {
+        display: inline-block;
+        flex-basis: 70px;
+        height: 40px;
+        vertical-align: top;
+        line-height: 40px;
+        color: #909399;
+        box-sizing: border-box;
+        background-color: #f5f7fa;
+        border: 1px solid #dcdfe6;
+        border-top-left-radius: 4px;
+        border-bottom-left-radius: 4px;
+        border-right: none;
+      }
+      input {
+        border-top-left-radius: 0;
+        border-bottom-left-radius: 0;
+      }
+      .el-input-number {
+        flex-grow: 1;
+      }
+    }
+    .num-label {
+      margin-right: 10px;
+    }
+    .pager {
+      margin-top: 20px;
+    }
   }
 </style>
