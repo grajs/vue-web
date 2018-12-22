925947efa5ef3f8df45ffcd3829e95e98ac62892
liujianhua <1769908045@qq.com>
2018-12-22 14:58:35 +0800

    fix：webpack-dev-serve关闭域名检查

--- build/webpack.config.js
+++ build/webpack.config.js
@@ -104,8 +104,10 @@ module.exports = {
     }
   },
   devServer: {
+    host:'0.0.0.0',
     port: 8080,
     hot: true,
+    disableHostCheck: true,
     contentBase: resolve('../dist'),
     proxy: {
       '/api': {
